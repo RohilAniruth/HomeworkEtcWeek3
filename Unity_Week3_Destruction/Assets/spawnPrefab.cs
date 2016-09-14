eb3e94aae4145bd9df63328f3ade3e0a534170fd
Rohil Aniruth <anirr579@newschool.edu>
2016-09-14 19:43:01 -0400

    Learned how to destroy and object via a trigger, and how to make an object appear on command. This will be used to make a space invader-esque game.

---
+++ Unity_Week3_Destruction/Assets/spawnPrefab.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnPrefab : MonoBehaviour
+{
+
+
+
+    public GameObject bluemisc;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //    if (Input.GetKeyDown( KeyCode.T))
+        //  {
+        //    Debug.Log("make tracks!");
+        //Spawn the prefab here
+        //  Instantiate(bluemisc, transform.position, transform.rotation);
+        //}
+        // }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GameObject temp = Instantiate(bluemisc, transform.position, transform.rotation) as GameObject;
+            temp.GetComponent<Rigidbody>().AddForce(Vecor3.up * 100);
+        }
+    }
+}
