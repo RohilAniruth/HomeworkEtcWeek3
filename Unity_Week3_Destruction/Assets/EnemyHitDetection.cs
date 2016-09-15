1dfe269475d39d569814558440141c2a9adfe855
Rohil Aniruth <anirr579@newschool.edu>
2016-09-15 18:26:40 -0400

    How to make the ship in Unity 3D shoot, and increase score - added basic UI.

---
+++ Unity_Week3_Destruction/Assets/EnemyHitDetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHitDetection : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter( Collider other)
+    {
+        Destroy(other.gameObject);
+        //get score object and add score
+
+        // scoreTracker.addScore();
+
+        GameObject scoreH = GameObject.Find("ScoreHolder");
+        scoreH.GetComponent<ScoreTracker>().addScore(324);
+
+        Destroy(gameObject); //destroy self
+    }
+	
+	}
